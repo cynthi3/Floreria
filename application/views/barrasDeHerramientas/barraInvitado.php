37f3021d68ae0a0f3d9bb93d4b08317df8476aea
Deybi Guevara <guevara_dey@hotmail.com>
2015-10-25 14:22:52 -0600

    barrita modificada

--- application/views/barrasDeHerramientas/barraInvitado.php
+++ application/views/barrasDeHerramientas/barraInvitado.php
@@ -7,19 +7,20 @@
         </a>
       </div>
 
-  <div class="one half" >
+  <div class="one half" align="right" >
    <div class="row">
-      <div class="three mobile twelfths skip-six">
+      
       <i class="icon-twitter icon-3x"></i>
       <i class="icon-facebook-sign icon-3x"></i>  
        <i class="icon-github icon-3x"></i> 
-      </div>
+      
   </div>
   </div>
 
 </div>
 <nav role="navigation" class="nav asphalt" title="Floreria">
   <ul>
+    <div class="one half">
     <li><a href="<?=base_url()?>" ><i class="icon-leaf"></i>  Floreria</a></li>
 
 
@@ -34,13 +35,14 @@
         <li><a href="<?=base_url()?>">Especiales</a></li>          <!--poner la funcion que dirige a la vista -->
       </ul>
   </li>
+</div>
 
+ <div class="one fourth two-up-small-tablet" ></div>
+      <div class="one fourth two-up-small-tablet">
+    <li ><a href="<?=base_url()?>">Registrate!</a></li>             <!--Add Funcion correcta -->
 
-
-    <li ><a href="<?=base_url()?>auth/register">Registrate!</a></li>             <!--Add Funcion correcta -->
-
-    <li ><a href="<?=base_url()?>auth/logout">Inicia Sesión!</a></li>        <!--Add Funcion correcta -->
-
+    <li ><a href="<?=base_url()?>">Inicia Sesión!</a></li>        <!--Add Funcion correcta -->
+</div>
   </ul>
 
 
